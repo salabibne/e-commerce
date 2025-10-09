a7c46ede7bac1bb6f816e794409ff19684ccafc0
salabibne <salabyaham@gmail.com>
2025-10-10 01:26:19 +0600

    swagger implemented

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Catalog API')
+    .setDescription(
+      'API for managing categories, subcategories, products, and variants',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+
+  await app.listen(3000);
 }
 bootstrap();
